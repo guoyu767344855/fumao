d7158baca47bd448aa0f72451943f4396ee2662d
郭昱 <guoyu@163.com>
2019-12-19 18:01:59 +0800

    修改会员中心

--- src/views/member/index.vue
+++ src/views/member/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class='member'>
-    <img src="../../assets/images/01.png" alt="">
-    <img src="../../assets/images/02.png" alt="">
+    <img src="../../assets/images/101.png" alt="">
+    <!-- <img src="../../assets/images/02.png" alt="">
     <img src="../../assets/images/03.png" alt="">
     <img src="../../assets/images/04.png" alt="">
     <img src="../../assets/images/05.png" alt="">
-    <img src="../../assets/images/06.png" alt="">
+    <img src="../../assets/images/06.png" alt=""> -->
   </div>
 </template>
 
